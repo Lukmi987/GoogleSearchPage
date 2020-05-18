5d63d9d80627f7ccc84a7b8132e1b4b9bfab3bc7
Lukas Komprs <komprs.l@gmail.com>
2020-05-18 17:32:11 +0200

    Adding ESLint for a static code analysis tool for identifying problematic patterns found in JavaScript code

--- main.js
+++ main.js
@@ -1,80 +1,79 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import { elements } from './views/base';
 
-
-//MY central variable
+s
+// MY central variable
 const state = {};
 
-//For testing 
+// For testing
 window.state = state;
 
-////// SEARCH CONTROLLER ///////
-const controlSearch = () => {  
-    //1) Display web and image div  
-    elements.imageContainer.style.visibility = ''; 
-    elements.webContainer.style.visibility = '';
+// SEARCH CONTROLLER //
+const controlSearch = () => {
+// 1) Display web and image div
+  elements.imageContainer.style.visibility = '';
+  elements.webContainer.style.visibility = '';
 
-    //2) Get query from input field 
-    const query = searchView.getInput();
-   
-    if(query){ 
-        //3) New Search object and add to state object 
-        state.search = new Search(query);
-              
-        //Clear previous results and display new results
-        clearResultsandRenderResults();    
-    }
-}
+  // 2) Get query from input field
+  const query = searchView.getInput();
+
+  if (query) {
+    // 3) New Search object and add to state object
+    state.search = new Search(query);
+
+    // Clear previous results and display new results
+    // eslint-disable-next-line no-use-before-define
+    clearResultsandRenderResults();
+  }
+};
 
 /**
- * 
+ *
  * @param startIndex To set start index for Api 'start' Query parameter
  */
-const clearResultsandRenderResults = async (startIndex=1) => {
-    //4) Prepare UI for results
-    searchView.clearInput();    
-    searchView.clearResult();
-   
-    //5) Search for query
-   try{ 
-        await  state.search.getResult(startIndex); 
-        const data = state.search.result
-    
-        //6) Render Results to UI
-        if (data != null) { //if undefined or null,//allowed coercion !=
-            searchView.renderAllWebResults(data);
-            searchView.renderAllImages(data);
-        }
-    } catch(error){
-    console.error(error);
+const clearResultsandRenderResults = async (startIndex = 1) => {
+  // 4) Prepare UI for results
+  searchView.clearInput();
+  searchView.clearResult();
+
+  // 5) Search for query
+  try {
+    await state.search.getResult(startIndex);
+    const data = state.search.result;
+
+    // 6) Render Results to UI
+    if (data != null) { // if undefined or null,//allowed coercion !=
+      searchView.renderAllWebResults(data);
+      searchView.renderAllImages(data);
     }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
 };
 
 
-
- /**
+/**
   * Submit functionality
   */
- elements.searchForm.addEventListener('submit',  e =>{
-    
-    //whenever we submit a form we prevent reloading the page
-    e.preventDefault();
+elements.searchForm.addEventListener('submit', (e) => {
+  // whenever we submit a form we prevent reloading the page
+  e.preventDefault();
 
-    controlSearch();
+  controlSearch();
 });
 
 
- /**
+/**
   * Pagination functionality
   */
- elements.pageList.addEventListener('click',  e=>{
-    e.preventDefault();  
-    const page = e.target.closest('.page'); 
-    const startindex = page.dataset.goto;   
-    
-    if (startindex && state.search !=null){ 
-        clearResultsandRenderResults(startindex);
-     }  
- });
+elements.pageList.addEventListener('click', (e) => {
+  e.preventDefault();
+  const page = e.target.closest('.page');
+  const startindex = page.dataset.goto;
 
+  if (startindex && state.search != null) {
+    clearResultsandRenderResults(startindex);
+  }
+});
--- views/searchView.js
+++ views/searchView.js
@@ -1,16 +1,18 @@
-import {elements} from './base';
+import { elements } from './base';
 
 /**
  * Sets the input value to empty string
  */
-export const clearInput = () => elements.searchInput.value = '';
+export const clearInput = () => {
+  elements.searchInput.value = '';
+};
 
 /**
  * Sets the innnerHtml  element property of Web cont. and Image cont. to empty value
  */
 export const clearResult = () => {
-    elements.webList.innerHTML = ''; 
-    elements.imageList.innerHTML = '';
+  elements.webList.innerHTML = '';
+  elements.imageList.innerHTML = '';
 };
 
 /**
@@ -21,10 +23,10 @@ export const getInput = () => elements.searchInput.value;
 
 /**
  * Inserts one web item to web results list before end
- * @param result contains result object 
+ * @param result contains result object
  */
-const renderOneWebResult = result =>{
-    const markup = `
+const renderOneWebResult = (result) => {
+  const markup = `
         <li>
             <a href="${result.formattedUrl}"><h4>${result.title}</h4></a>
             <a class="results__link" href="${result.formattedUrl}">${result.htmlFormattedUrl}</a>
@@ -34,27 +36,27 @@ const renderOneWebResult = result =>{
 
         </li>
     `;
-    elements.webList.insertAdjacentHTML('beforeend',markup);
+  elements.webList.insertAdjacentHTML('beforeend', markup);
 };
 
 /**
  * Inserts one image to Image list before end
  * @param result contains specific path for the image
  */
-const renderImage = result =>{
-    const markup = ` 
+const renderImage = (result) => {
+  const markup = ` 
             <img src="${result}" alt="" class="web__img">     
     `;
 
-    elements.imageList.insertAdjacentHTML('beforeend',markup); 
-} 
+  elements.imageList.insertAdjacentHTML('beforeend', markup);
+};
 
 /**
  * Loops through our result array
  * @param searchResult contains whole result object of our search query
  */
-export const renderAllWebResults = (searchResult) =>{
-    searchResult.forEach(renderOneWebResult);  
+export const renderAllWebResults = (searchResult) => {
+  searchResult.forEach(renderOneWebResult);
 };
 
 
@@ -62,17 +64,15 @@ export const renderAllWebResults = (searchResult) =>{
  *  First Loop selects a web result and second selects a source path to the img
  * @param searchResult contains whole result object of our search query
  */
-export const renderAllImages = (searchResult) =>{    
-        searchResult.forEach( el => { 
-                if (el.pagemap != null) { 
-                    if (el.pagemap.cse_thumbnail != null) { //Uncaught TypeError: Cannot read property  of undefined 
-                        el.pagemap.cse_thumbnail.forEach( el => {
-                            renderImage(el.src); 
-                        }); 
-                    }
-                }
+export const renderAllImages = (searchResult) => {
+  searchResult.forEach((el) => {
+    if (el.pagemap != null) {
+      // Uncaught TypeError: Cannot read property  of undefined
+      if (el.pagemap.cse_thumbnail != null) {
+        el.pagemap.cse_thumbnail.forEach((element) => {
+          renderImage(element.src);
         });
-        
+      }
+    }
+  });
 };
-
-
